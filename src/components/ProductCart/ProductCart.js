7811196f94e269b4ce43ea0cbd0a14890d218b23
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-01-09 19:31:10 +0600

    fatch products from api and showing home page

--- src/components/ProductCart/ProductCart.js
+++ src/components/ProductCart/ProductCart.js
@@ -11,51 +11,74 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ProductCart = () => {
-  const [value, setValue] = React.useState(2);
+const ProductCart = ({ product, loading }) => {
   return (
     <Grid item xs={12} sm={4} md={3} lg={2.4}>
-      <Card
-        sx={{
-          maxWidth: 345,
-          border: "1px solid",
-          borderColor: "#bcdcdc",
-          boxShadow: "none",
-          "&:hover": {
+      <Link to={`/product-details/${product.id}`}>
+        <Card
+          sx={{
+            maxWidth: 345,
+            height: 345,
+            border: "1px solid",
+            borderColor: "#bcdcdc",
+            boxShadow: "none",
+            position: "relative",
+            "&:hover": {
               borderColor: "#008ECC",
-              boxShadow: "0px 0px 4px"
-          },
-        }}
-      >
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="140"
-          image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h6">
-            Galaxy s22 Ultra
-          </Typography>
-          <Typography gutterBottom variant="body2">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit...
-          </Typography>
-          <Box display="flex" justifyContent="space-between">
-            <Typography variant="body2" fontWeight="bold">
-              $32999
+              boxShadow: "0px 0px 4px",
+            },
+          }}
+        >
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image={product.image}
+            sx={{ objectFit: "contain" }}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="body"
+              fontWeight={600}
+              fontSize="16px"
+              pb={1}
+              display="inline-block"
+            >
+              {product.title.substring(0, 15)}
             </Typography>
-            <Typography variant="body2">
-              <Rating name="read-only" value={2} readOnly size="small" />
+            <Typography gutterBottom variant="body2">
+              {product.description.substring(0, 30)}...
             </Typography>
-          </Box>
-        </CardContent>
-        <CardActions>
-          <Button size="small" fullWidth endIcon={<ShoppingCartOutlinedIcon />}>
-            Add To Cart
-          </Button>
-        </CardActions>
-      </Card>
+            <Box display="flex" justifyContent="space-between">
+              <Typography variant="body2" fontWeight="bold">
+                ${product.price}
+              </Typography>
+              <Typography variant="body2">
+                <Rating
+                  name="read-only"
+                  value={product.rating.rate}
+                  readOnly
+                  size="small"
+                />
+              </Typography>
+            </Box>
+          </CardContent>
+          <CardActions
+            sx={{ position: "absolute", bottom: "0", width: "100%" }}
+          >
+            <Button
+              size="small"
+              fullWidth
+              endIcon={<ShoppingCartOutlinedIcon />}
+            >
+              Add To Cart
+            </Button>
+          </CardActions>
+        </Card>
+      </Link>
     </Grid>
   );
 };
--- src/pages/Home/SmartPhone.js
+++ src/pages/Home/SmartPhone.js
@@ -1,25 +1,34 @@
 import { Box, Container, Grid } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeadingContainer from "../../components/HeadingContainer/HeadingContainer";
 import ProductCart from "../../components/ProductCart/ProductCart";
 
 const SmartPhone = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  //fetch products
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setProducts(data);
+      });
+  }, []);
+
   return (
     <Box>
       <HeadingContainer title="Grab the best on Smartphones" url="/" />
+
       <Box mt={4}>
         <Container>
           <Grid container spacing={2}>
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
-            <ProductCart />
+            {loading
+              ? "loading..."
+              : products?.map((product) => (
+                  <ProductCart product={product} key={product.id} />
+                ))}
           </Grid>
         </Container>
       </Box>
